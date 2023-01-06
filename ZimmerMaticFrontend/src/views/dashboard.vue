47133df762fc80cb7e9b2c345030622b78fb9ddb
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-01-06 11:44:02 +0100

    ++

--- ZimmerMaticFrontend/src/views/dashboard.vue
+++ ZimmerMaticFrontend/src/views/dashboard.vue
@@ -55,11 +55,10 @@
               <div class="card-header">Serverstatus</div>
               <div class="card-body">
                 <p>Systemload 0.29</p>
-                <p>Speicherbelegung: 30% </p>
+                <p>Speicherbelegung: 30%</p>
                 <p>CPU 10%</p>
                 <p>Temperature: 49,7°</p>
                 <p>RAM 10%</p>
-                <p>Users logged in 0</p>
                 <p>Backend Online</p>
                 <p>MQTT Connections: 8</p>
                 <p>MQTT Topics: 8</p>
@@ -93,18 +92,59 @@
             <div class="card h-100">
               <div class="card-header">Services</div>
               <div class="card-body">
-                <p>Openhab Online</p>
-                <p>Portainer Online</p>
-                <p>EMQX Online</p>
-                <p>Pocketbase Online</p>
-                <p>MongoDB Offline</p>
+                <p>
+                  Openhab
+                  <span
+                    v-if="this.openhab == 200"
+                    class="badge rounded-pill bg-success"
+                    >Online</span
+                  >
+                  <span v-else class="badge rounded-pill bg-danger"
+                    >Offline</span
+                  >
+                </p>
+                <p>
+                  Portainer Online
+                  <span
+                    v-if="this.portainer == 200"
+                    class="badge rounded-pill bg-success"
+                    >Online</span
+                  >
+                  <span v-else class="badge rounded-pill bg-danger"
+                    >Offline</span
+                  >
+                </p>
+                <p>
+                  EMQX Online
+                  <span
+                    v-if="this.emqx == 200"
+                    class="badge rounded-pill bg-success"
+                    >Online</span
+                  >
+                  <span v-else class="badge rounded-pill bg-danger"
+                    >Offline</span
+                  >
+                </p>
+                <p>
+                  Pocketbase
+                  <span class="badge rounded-pill bg-danger">Offline</span>
+                </p>
+                <p>
+                  MongoDB
+                  <span
+                    v-if="this.mongo == 200"
+                    class="badge rounded-pill bg-success"
+                    >Online</span
+                  >
+                  <span v-else class="badge rounded-pill bg-danger"
+                    >Offline</span
+                  >
+                </p>
               </div>
             </div>
           </div>
           <div class="col-lg-4">
-            <div class="card h-100">
-              
-            </div>
+            <div class="card h-100"></div>
           </div>
         </div>
       </div>
@@ -117,15 +157,36 @@
 import Navbardark from "../components/navbardark.vue";
 import LED from "../components/LEDStatusKachel.vue";
 import LIGHT from "../components/LampeStatusKachel.vue";
+import axios from "axios";
 export default {
+  data() {
+    return {
+      openhab: 0,
+      portainer: 0,
+      emqx: 0,
+      mongo: 0,
+    };
+  },
   components: {
     Navbardark,
     LED,
     LIGHT,
   },
-  methods: {},
-  mounted() {
+  methods: {
+    async get_state(ip){
+      let res = await axios
+      .get(ip)
+      .then((response) => response.status);
+      return res
+    },
+  },
+  async mounted() {
     this.timer = setInterval(this.fetchEventsList, 30000);
+    this.openhab = await this.get_state("http://192.168.0.138:8080/rest/items")
+    console.log(this.openhab);
+    this.mongo = await this.get_state("http://192.168.0.138:42069/")
+    this.emqx = await this.get_state("http://192.168.0.138:18083/status")
+    this.portainer = await this.get_state("http://192.168.0.138:9000/")
   },
 };
 </script>
