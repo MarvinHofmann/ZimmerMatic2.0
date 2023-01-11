1a2189d8979df782fb86df46156b3693e375752b
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-01-11 21:50:19 +0100

    ++

--- ZimmerMaticBackend/OTA/OTA.js
+++ ZimmerMaticBackend/OTA/OTA.js
@@ -6,7 +6,7 @@ router.get("/software", function (req, res) {
     console.log("HIHI");
     p = path.join(__dirname)
     console.log(p);
-    res.download(p + '/Blink.ino.bin', (err)=>{
+    res.download(p + '/Blink.bin', (err)=>{
         if (err) {
             console.error("Problem on download firmware: ", err)
         }else{
