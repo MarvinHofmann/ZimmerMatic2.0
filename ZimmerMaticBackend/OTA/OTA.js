5c02e3adc6db0209bc04a2639d14e0f739ff037d
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-01-12 21:02:39 +0100

    ++

--- ZimmerMaticBackend/OTA/OTA.js
+++ ZimmerMaticBackend/OTA/OTA.js
@@ -16,6 +16,19 @@ router.get('/update', (request, response) => {
     console.log('Your file has been downloaded ' + downloadCounter + ' times!')
 })
 
+router.get('/update/server', (request, response) => {
+    console.log(request.headers);
+    console.log(request.body);
+    response.download(path.join(__dirname, 'firmware/Server.bin'), 'Server.bin', (err) => {
+        if (err) {
+            console.error("Problem on download firmware: ", err)
+        } else {
+            downloadCounter++;
+        }
+    });
+    console.log('Your file has been downloaded ' + downloadCounter + ' times!')
+})
+
 router.get('/ping', (request, res) => {
     console.log(request.headers);
     console.log(request.body);
