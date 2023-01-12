69d14a9bb748334616ad3c04a8da16be4c947eb8
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-01-12 18:05:08 +0100

    Update OTA.js

--- ZimmerMaticBackend/OTA/OTA.js
+++ ZimmerMaticBackend/OTA/OTA.js
@@ -2,18 +2,6 @@ const router = require('express').Router();
 module.exports = router
 const path = require('path');
 
-router.get("/software", function (req, res) {
-    console.log("HIHI");
-    p = path.join(__dirname)
-    console.log(p);
-    res.download(p + '/Blink.bin', (err)=>{
-        if (err) {
-            console.error("Problem on download firmware: ", err)
-        }else{
-        }
-    });
-});
-
 let downloadCounter = 1;
 router.get('/update', (request, response) => {
     console.log(request.headers);
